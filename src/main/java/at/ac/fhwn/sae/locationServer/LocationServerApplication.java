ae18151b0f3db903488ea32541fb46d82b73b372
Andreas Tauböck <andreas.tauboeck@josephinum.at>
2023-01-17 13:54:16 +0100

    test hinzugefügt

--- src/main/java/at/ac/fhwn/sae/locationServer/LocationServerApplication.java
+++ src/main/java/at/ac/fhwn/sae/locationServer/LocationServerApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocationServerApplication {
     public static void main(String[] args){
         SpringApplication.run(LocationServerApplication.class, args);
+        //Das ist ein Test
     }
 }
